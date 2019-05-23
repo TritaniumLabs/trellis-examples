5f53709c6a4d3792dbbb44a3027dcc5924613711
angularEd <ed@grocerystoredemos.com>
2019-05-23 15:57:02 -0500

    PHP /createAddress curl demo using GET

--- php_create_address2.php
+++ php_create_address2.php
@@ -12,6 +12,7 @@ This example is equivalent to using the browser and navigating to:
 https://traceabilityapi.com/createAddress/?auth_key=31a151d30363396042c3d1977a5763b18b90cb7f95192b9f06e7824c626862c1:996048c0f3dff6d8dd8d6527e9184c9ae91005d33a92fccad4d8d2ca625a14a1&address=1600+Golf+Rd+Rolling+Meadows+IL27b4245994aa08e837d07421f0e18e478f622e2422bc3ce475690b16d6190ee9//==========================================================================
 
 */
+//
 //-- Replace with your data.
 
 $hash="27b4245994aa08e837d07421f0e18e478f622e2422bc3ce475690b16d6190ee9";
