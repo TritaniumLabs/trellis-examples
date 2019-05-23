bb41e4105e7c8dafc9cbf17c5f0bb70349402572
angularEd <ed@grocerystoredemos.com>
2019-05-23 16:03:09 -0500

    PHP /trellisGet demo using curl and GET

--- php_trellisHistory2.php
+++ php_trellisHistory2.php
@@ -11,7 +11,6 @@ https://traceabilityapi.com/trellisHistory/?auth_key=31a151d30363396042c3d1977a5
 */
 //==========================================================================
 
-
 $hash="27b4245994aa08e837d07421f0e18e478f622e2422bc3ce475690b16d6190ee9";
 $auth_key="31a151d30363396042c3d1977a5763b18b90cb7f95192b9f06e7824c626862c1:996048c0f3dff6d8dd8d6527e9184c9ae91005d33a92fccad4d8d2ca625a14a1";
 
@@ -23,5 +22,6 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $result = curl_exec($ch);
 curl_close($ch);
 echo $result;
+
 
 ?>
